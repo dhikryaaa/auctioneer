a310d78eeb4387b6c3ac226fa6a4a5779c790943
dhikryaaa <andhika.aryaputra@gmail.com>
2026-06-08 18:19:11 +0700

    refactor: update models to mapped

--- services/bid/models.py
+++ services/bid/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 class Bid(Base):
     __tablename__ = "bids"
 
-    id         = Column(Integer, primary_key=True, autoincrement=True)
-    auction_id = Column(Integer, nullable=False, index=True)
-    bidder_id  = Column(Integer, nullable=False, index=True)
-    amount     = Column(Integer, nullable=False)
-    placed_at  = Column(DateTime, default=datetime.utcnow, nullable=False)
+    id:         Mapped[int]      = mapped_column(primary_key=True, autoincrement=True)
+    auction_id: Mapped[int]      = mapped_column(index=True)
+    bidder_id:  Mapped[int]      = mapped_column(index=True)
+    amount:     Mapped[int]      = mapped_column()
+    placed_at:  Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    
